39e0c8f8db8e4bd1ff8435ab3ce892a1ad9e14f0
arzoo995 <c4016221@shu.ac.uk>
2024-12-15 20:15:59 +0000

    co

--- js/main.js
+++ js/main.js
@@ -4,3 +4,6 @@ function toggleMenu() {
   navLinks.style.right = navLinks.style.right === "0" ? "-200px" : "0";
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to Cantor College!");
+});
